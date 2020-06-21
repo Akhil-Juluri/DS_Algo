ed3a7f66fef3986dbc91587a5fde3d7abd24af6a
Akhil-Juluri <akhiljuluri@gmail.com>
2020-06-21 23:35:47 +0530

    Update GeneratingPermutationsOfArrayRecursively.java

--- GeneratingPermutationsOfArrayRecursively.java
+++ GeneratingPermutationsOfArrayRecursively.java
@@ -2,7 +2,7 @@ public class Permutation
 { 
 	public static void main(String[] args) 
 	{  
-    int arr[] = {1,2,3};
+    		int arr[] = {1,2,3};
 		Permutation permutation = new Permutation(); 
 		permutation.permute(arr, 0, arr.length - 1); 
 	} 
@@ -10,24 +10,22 @@ public class Permutation
 	{ 
 		if (l == r) {
 			for (int i = 0; i < arr.length; i++) {
-          System.out.print(arr[i]+" ");
-      }
-      System.out.println();
-    }
+          			System.out.print(arr[i]+" ");
+      			}
+      			System.out.println();
+    		}
 		else
-		{ 
-			for (int i = l; i <= r; i++) 
-			{ 
+		{
+			for (int i = l; i <= r; i++)
+			{
 				int temp = arr[l];
-        arr[l] = arr[i];
-        arr[i] = temp;
-              
+        			arr[l] = arr[i];
+        			arr[i] = temp;
 				permute(arr, l+1, r); 
-              
 				temp = arr[l];
-        arr[l] = arr[i];
-        arr[i] = temp;
-			} 
-		} 
-	} 
+        			arr[l] = arr[i];
+        			arr[i] = temp;
+			}
+		}
+	}
 }
