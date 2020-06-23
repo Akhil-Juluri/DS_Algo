60d298a9a0449a539b0bcc77a09dd3b3ee00464e
Akhil-Juluri <akhiljuluri@gmail.com>
2020-06-23 16:54:17 +0530

    Update MinOrMaxSumSubArrayOfSizeK.java

--- MinOrMaxSumSubArrayOfSizeK.java
+++ MinOrMaxSumSubArrayOfSizeK.java
@@ -49,3 +49,13 @@ class Codechef
 		System.out.println(minSumSubarrayOfSizeK(nums, k));
 	}
 }
+/*
+
+Input: [2, 1, 5, 1, 3, 2], k=3 
+Output: 9
+Explanation: Subarray with maximum sum is [5, 1, 3].
+
+Input: [2, 3, 4, 1, 5], k=2 
+Output: 7
+Explanation: Subarray with maximum sum is [3, 4].
+*/
