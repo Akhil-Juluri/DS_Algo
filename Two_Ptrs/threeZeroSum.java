8963114c8d375d3127e9617e41314e38a83e44c2
Akhil-Juluri <akhiljuluri@gmail.com>
2020-07-02 17:18:19 +0530

    Update threeZeroSum.java

--- Two_Ptrs/threeZeroSum.java
+++ Two_Ptrs/threeZeroSum.java
@@ -5,12 +5,12 @@ public class Solution {
         HashSet<String> hs = new HashSet<String>();
         for (int i = 0; i < nums.size(); i++) {
             int j = i + 1, k = nums.size() - 1;
+            long x = nums.get(i);
             while (j < k) {
-                long x = nums.get(i);
                 long y = nums.get(j);
                 long z = nums.get(k);
                 long sum = x + y + z;
-                if (sum == 0) {
+                if (sum == 0) { // or we could also check for y + z == -x because if x + y + z = 0 then y + z = 0 - x = -x
                     ArrayList<Integer> currRes = new ArrayList<Integer>();
                     currRes.add(nums.get(i));
                     currRes.add(nums.get(j));
