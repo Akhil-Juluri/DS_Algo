b584bc51da4a4606a697cc5072ce2f15c2f6588e
Akhil-Juluri <akhiljuluri@gmail.com>
2020-07-25 17:26:53 +0530

    Update and rename twoSum.java to Two_Ptrs/twoSum.java

--- Two_Ptrs/twoSum.java
+++ Two_Ptrs/twoSum.java
@@ -7,7 +7,7 @@ import java.io.*;
 /* Name of the class has to be "Main" only if the class is public. */
 class Codechef
 {
-    public static ArrayList<ArrayList<Integer>> twoSum(int[] nums, int target) {
+    public static ArrayList<ArrayList<Integer>> twoSumForUnsortedArray(int[] nums, int target) {
         ArrayList<ArrayList<Integer>> resArr = new ArrayList<ArrayList<Integer>>();
         HashMap<Integer,Integer> hs = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
@@ -21,7 +21,26 @@ class Codechef
             }
         }
         //System.out.println(resArr);
+         // return indices of all pairs that adds to target
         return resArr;
+    }
+    public static ArrayList<Integer> twoSumForSortedArray(int[] nums, int target) {
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        int left = 0, right = nums.length - 1, sum = 0;
+        while (left < right) {
+            sum = nums[left] + nums[right];
+            if (sum == target) {
+                res.add(nums[left]);
+                res.add(nums[right]);
+                return res;
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        // returns a single pair of values that adds to target or empty if no such pair exists
+        return res;
     }
 	public static void main (String[] args) throws java.lang.Exception
 	{
@@ -33,7 +52,10 @@ class Codechef
 		    arr[i] = sc.nextInt();
 		}
 		int target = sc.nextInt();
-		ArrayList<ArrayList<Integer>> res = twoSum(arr, target);
+		ArrayList<ArrayList<Integer>> res = twoSumForUnsortedArray(arr, target);
 		System.out.println(res);
+		Arrays.sort(arr);
+		ArrayList<Integer> res1 = twoSumForSortedArray(arr, target);
+		System.out.println(res1);
 	}
 }
