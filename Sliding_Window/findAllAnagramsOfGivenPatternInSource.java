345143b0b847b1dc572de2470c31fbab151bab3f
Akhil-Juluri <akhiljuluri@gmail.com>
2020-08-03 21:19:03 +0530

    Update findAllAnagramsOfGivenPatternInSource.java

--- Sliding_Window/findAllAnagramsOfGivenPatternInSource.java
+++ Sliding_Window/findAllAnagramsOfGivenPatternInSource.java
@@ -28,21 +28,36 @@ class Codechef
         int start = 0, startIndex = -1, endIndex = -1, continuous = 0;
         for (int end = 0; end < n; end++) {
             char ch = source.charAt(end);
+	    /* if character is present in HashMap then decrement it's frequency by 1
+            as and if it's frequency is 0 i.e we have matched all of it's frequency
+            from the pattern so if it's frequency is 0 increment continuous i.e continuously
+            we have found a character of pattern in the String, if frequency goes
+            negative it means currently included character is a part of pattern*/
             if (hm.containsKey(ch)) {
                 hm.put(ch, hm.get(ch)-1);
                 if (hm.get(ch) == 0) {
                     continuous++;
                 }
             }
+	    /* HashMapsize is nothing but no. of unique characters in the pattern
+            if we also continuously match those many characters then we got our ans*/
             if (continuous == hm.size()) {
                 endIndex = end; // storing endIndex of any permutation of the pattern in source
                 //startIndex = end - k + 1; // end - k + 1 == start so we can use start also // storing startIndex of any permutation of the pattern in source
-		        startIndex = start;
-		        startIndices.add(startIndex);
+		startIndex = start;
+		startIndices.add(startIndex);
                 //System.out.println(startIndex+"  "+endIndex);
                 
             }
-            if (end >= k - 1) {
+	    /* if current window length exceeds length of pattern then 
+            take character at start and if it is present in hashmap
+            increment it's frequency by 1 as we have already decremented it in
+            above if statement i.e we are putting it back in the hashmap 
+            but before putting it back in hashmap check if it's frequency is 0
+            if it is 0 that means it got completely matched in above if statement
+            and we have incremented continuous value, but as now it is 0 it got 
+            unmatched so decrement continuous value and add a frequency of 1 to it in hashmap*/
+            if (end - start + 1 >= k) {
                 char c = source.charAt(start);
                 if (hm.containsKey(c)) {
                     if (hm.get(c) == 0) {
